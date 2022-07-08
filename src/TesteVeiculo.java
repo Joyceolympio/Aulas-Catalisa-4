d0a1d995229c42d641fb482c59ab5713a546eda5
Joyceolympio <markathere@hotmail.com>
2022-07-08 13:15:55 -0300

    Códigos aula-20

---
+++ src/TesteVeiculo.java
@@ -0,0 +1,11 @@
+public class TesteVeiculo {
+
+    public static void main(String[] args) {
+
+        Veiculo veiculo = new Veiculo();
+
+        veiculo.setMotor("motor tal");
+        System.out.println(veiculo.getMotor());
+
+    }
+}
---
+++ src/Veiculo.java
@@ -0,0 +1,39 @@
+public class Veiculo {
+
+    private String motor;
+    private String combustivel;
+    private String marca;
+    private String cor;
+
+    public String getMotor() {
+        return motor;
+    }
+
+    public void setMotor(String motor) {
+        this.motor = motor;
+    }
+
+    public String getCombustivel() {
+        return combustivel;
+    }
+
+    public void setCombustivel(String combustivel) {
+        this.combustivel = combustivel;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+}
